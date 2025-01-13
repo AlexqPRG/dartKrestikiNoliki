af947705e705afd73f9441d8e7a5b9b2d6b09ee2
Myts A.V <myts.shura@mail.ru>
2025-01-13 16:38:03 +0300

    FINAL

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -56,51 +56,132 @@ RobotClass robotLogic(){
   return robot;
 }
 
-///функция для проверки победы
-bool checkWin(){ 
-  for (int i = 0; i < massive.length; i++){
-    for(int j = 0; j < massive.length - 2; j++){
-      //условие для проверки строк
-      if(massive[i][j] != "." && massive[i][j] == massive[i][j+1] && massive[i][j] == massive[i][j+2]){
-        whoWin = massive[i][j] == "X" ? 1 : 2;
-        return true;
+// ///функция для проверки победы
+// bool checkWin(){ 
+//   for (int i = 0; i < massive.length; i++){
+//     for(int j = 0; j < massive.length - 2; j++){
+//       //условие для проверки строк
+//       if(massive[i][j] != "." && massive[i][j] == massive[i][j+1] && massive[i][j] == massive[i][j+2]){
+//         whoWin = massive[i][j] == "X" ? 1 : 2;
+//         return true;
+//       }
+//       //условие для проверки столбцов
+//       if(massive[j][i] != "." && massive[j][i] == massive[j+1][i] && massive[j][i] == massive[j+2][i]){
+//         whoWin = massive[j][i] == "X" ? 1 : 2;
+//         return true;
+//       }
+//     }
+//   }
+
+// //проверяем диагональ слева направо
+//   for(int i = 0; i < massive.length-2; i++){
+//     for(int j = 0; j < massive.length-2; j++){
+//       if(massive[i][j] != "." && massive[i][j] == massive[i+1][j+1] && massive[i][j] == massive[i+2][j+2]){
+//         whoWin = massive[i][j] == "X" ? 1 : 2;
+//         return true;
+//       }
+//     }
+//   }
+
+// //проверяем диагональ справа налево
+//   for(int i = 0; i < massive.length - 2; i++){
+//     for(int j = 2; j < massive.length; j++){
+//       if(massive[i][j] != "." && massive[i][j] == massive[i+1][j-1] && massive[i][j] == massive[i+2][j-2]){
+//         whoWin = massive[i][j] == "X" ? 1 : 2;
+//         return true;
+//       }
+//     }
+//   }
+
+//   if(drawCheck()){
+//     return true;
+//   }
+
+
+
+//   return false;
+// }
+
+bool checkWin() {
+  // Проверяем строки
+  for (int i = 0; i < massive.length; i++) {
+    bool row = true;
+    String firstChar = massive[i][0];
+    if (firstChar != ".") { // Если первый символ не "." проверяем остальные
+      for (int j = 1; j < massive[i].length; j++) {
+        if (massive[i][j] != firstChar) {
+          row = false;
+          break;
+        }
       }
-      //условие для проверки столбцов
-      if(massive[j][i] != "." && massive[j][i] == massive[j+1][i] && massive[j][i] == massive[j+2][i]){
-        whoWin = massive[j][i] == "X" ? 1 : 2;
+      if (row) {
+        whoWin = firstChar == "X" ? 1 : 2;
         return true;
       }
     }
   }
 
-//проверяем диагональ слева направо
-  for(int i = 0; i < massive.length-2; i++){
-    for(int j = 0; j < massive.length-2; j++){
-      if(massive[i][j] != "." && massive[i][j] == massive[i+1][j+1] && massive[i][j] == massive[i+2][j+2]){
-        whoWin = massive[i][j] == "X" ? 1 : 2;
+  // Проверяем столбцы
+  for (int i = 0; i < massive.length; i++) {
+    bool col = true;
+    String firstChar = massive[0][i];
+    if (firstChar != ".") { // Если первый символ не "." проверяем остальные
+      for (int j = 1; j < massive.length; j++) {
+        if (massive[j][i] != firstChar) {
+          col = false;
+          break;
+        }
+      }
+      if (col) {
+        whoWin = firstChar == "X" ? 1 : 2;
         return true;
       }
     }
   }
 
-//проверяем диагональ справа налево
-  for(int i = 0; i < massive.length - 2; i++){
-    for(int j = 2; j < massive.length; j++){
-      if(massive[i][j] != "." && massive[i][j] == massive[i+1][j-1] && massive[i][j] == massive[i+2][j-2]){
-        whoWin = massive[i][j] == "X" ? 1 : 2;
-        return true;
+  // Проверяем диагонали слева направо
+  bool diag1 = true;
+  String Diag1 = massive[0][0];
+  if (Diag1 != ".") { 
+    for (int i = 1; i < massive.length; i++) {
+      if (massive[i][i] != Diag1) {
+        diag1 = false;
+        break;
       }
+    }
+    if (diag1) {
+      whoWin = Diag1 == "X" ? 1 : 2;
+      return true;
     }
   }
 
-  if(drawCheck()){
+  // Проверяем диагонали справа налево
+  bool diag2 = true;
+  String Diag2 = massive[0][massive.length - 1];
+  if (Diag2 != ".") { 
+    for (int i = 1; i < massive.length; i++) {
+      if (massive[i][massive.length - 1 - i] != Diag2) {
+        diag2 = false;
+        break;
+      }
+    }
+    if (diag2) {
+      whoWin = Diag2 == "X" ? 1 : 2;
+      return true;
+    }
+  }
+
+  if (drawCheck()) {
     return true;
   }
 
-
-
   return false;
 }
+
+
+
+
+
 ///функция для вывода красивого поля со значениями
 void printPole(){ 
   var lenght = massive.length;
@@ -121,8 +202,6 @@ void printPole(){
 }
 
 void game(){
-
-  
   random.nextInt(2) == 0 ? isKrestiki = true : isKrestiki = false;
   whoWin = 0;
   isGame = !isGame;
